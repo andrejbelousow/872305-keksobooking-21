b6b50a04c4fcdfaf497ed23b9ec2ba7a0270c973
Andrey <andr.belousow2017@yandex.ru>
2020-10-08 00:28:30 +0300

    прдолжение работы над первой задачей

--- js/main.js
+++ js/main.js
@@ -1,50 +1,67 @@
 const OFFERS_TYPES = [`palace`, `flat`, `house`, `bungalow`];
-const CHECK_TIME = [`12:00`, `13:00`, `14:00`];
-const OFFER_FEATURES = [`wifi`, `dishwasher`, `parking`, `washer`, `elevator`, `conditioner`];
-const OFFER_PHOTOS = [`http://o0.github.io/assets/images/tokyo/hotel1.jpg`, `http://o0.github.io/assets/images/tokyo/hotel2.jpg`, `http://o0.github.io/assets/images/tokyo/hotel3.jpg`];
+const CHECK_TIMES = [`12:00`, `13:00`, `14:00`];
+const OFFER_FEATURES = [
+  `wifi`,
+  `dishwasher`,
+  `parking`,
+  `washer`,
+  `elevator`,
+  `conditioner`,
+];
+const OFFER_PHOTOS = [
+  `http://o0.github.io/assets/images/tokyo/hotel1.jpg`,
+  `http://o0.github.io/assets/images/tokyo/hotel2.jpg`,
+  `http://o0.github.io/assets/images/tokyo/hotel3.jpg`,
+];
+const MAP_WIDTH = 1200;
 const generatedObjects = [];
 
-const getRandomValue = function(maxValue, minValue = 0) {
-  return Math.floor(Math.random() * (maxValue - minValue)) + minValue;
-};
-
 const createObjectArray = function() {
+  const getRandomValue = function(maxValue, minValue = 0) {
+  return Math.floor(Math.random() * (maxValue - minValue)) + minValue;
+  };
+
   for (let i = 1; i <= 8; i++) {
     generatedObjects.push({
-      author: {
-        avatar: `img/avatars/user0${i}.png`,
+      "author": {
+        "avatar": `img/avatars/user0${i}.png`,
       },
-      offer: {
-        title: `Невероятное предложение`,
-        address: `${this.location.x}, ${this.location.y}`,
-        price: 6500,
-        type: OFFERS_TYPES[getRandomValue(OFFERS_TYPES.length)],
-        rooms: 2,
-        guests: 3,
-        checkin: CHECK_TIME[getRandomValue(CHECK_TIME.length)],
-        checkout: CHECK_TIME[getRandomValue(CHECK_TIME.length)],
-        features: OFFER_FEATURES.slice(
-          0,
-          getRandomValue(OFFER_FEATURES.length)
-        ),
-        description: `Аппартаменты рядом с центральной площадью, покушать можно совсем недалеко`,
-        photos: OFFER_PHOTOS.slice(0, getRandomValue(OFFER_PHOTOS.length)),
+      "location": {
+        "x": getRandomValue(MAP_WIDTH),
+        "y": getRandomValue(630, 130),
       },
-      location: {
-        x: getRandomValue(120),
-        y: getRandomValue(630, 130),
-      },
+      "offer": {
+        "title": `Невероятное предложение`,
+        "address": `600, 350`,
+        "price": 6500,
+        "type": OFFERS_TYPES[getRandomValue(OFFERS_TYPES.length)],
+        "rooms": 2,
+        "guests": 3,
+        "checkin": CHECK_TIMES[getRandomValue(CHECK_TIMES.length)],
+        "checkout": CHECK_TIMES[getRandomValue(CHECK_TIMES.length)],
+        "features": OFFER_FEATURES.slice(getRandomValue(OFFER_FEATURES.length)),
+        "description": `Аппартаменты рядом с центральной площадью, покушать можно совсем недалеко`,
+        "photos": OFFER_PHOTOS.slice(getRandomValue(OFFER_PHOTOS.length)),
+      }
     });
   }
 };
 
-createObjectArray();
+const showMap = function () {
+  document.querySelector(".map").classList.remove("map--faded");
+};
 
-console.log(
-  generatedObjects[0].offer.address,
-  generatedObjects[0].offer.type,
-  generatedObjects[0].offer.checkin,
-  generatedObjects[0].offer.checkout,
-  generatedObjects[0].offer.features,
-  generatedObjects[0].offer.photos
-);
+const createAndRenderPins = function () {
+  const PIN_TEMPLATE = document
+    .querySelector("#pin")
+    .content.querySelector(".map__pin");
+
+    for (let i = 0; i < generatedObjects.length; i++) {
+      const pinElement = PIN_TEMPLATE.cloneNode(true);
+      const objectItem = generatedObjects[i];
+      pinElement.style = `left: ${objectItem.}px; top: ${}`
+    }
+};
+
+createObjectArray();
+showMap();
