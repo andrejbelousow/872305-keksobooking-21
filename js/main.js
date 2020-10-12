5d84e776d364b880736aa72f9ecf47e609d84948
andrejbelousow <45243929+andrejbelousow@users.noreply.github.com>
2020-10-12 17:19:45 +0300

    Начал исправления

--- js/main.js
+++ js/main.js
@@ -15,23 +15,23 @@ const OFFER_PHOTOS = [
   `http://o0.github.io/assets/images/tokyo/hotel2.jpg`,
   `http://o0.github.io/assets/images/tokyo/hotel3.jpg`,
 ];
-const generatedObjects = [];
 
-const PIN_TEMPLATE = document
+const pinTemplate = document
   .querySelector(`#pin`)
   .content.querySelector(`.map__pin`);
 const PIN_OFFSET_X = 50;
 const PIN_OFFSET_Y = 70;
 const MAP_WIDTH = 1200 - PIN_OFFSET_X - PIN_OFFSET_X;
-const PINS_MAP = document.querySelector(`.map__pins`);
+const pinsMap = document.querySelector(`.map__pins`);
 
-const createObjectArray = function () {
+const createMocksArray = function () {
   const getRandomValue = function (maxValue, minValue = 0) {
     return Math.floor(Math.random() * (maxValue - minValue)) + minValue;
   };
 
+  const generatedMocks = [];
   for (let i = 1; i <= 8; i++) {
-    generatedObjects.push({
+    generatedMocks.push({
       "author": {
         "avatar": `img/avatars/user0${i}.png`,
       },
@@ -54,33 +54,34 @@ const createObjectArray = function () {
       }
     });
   }
+  return generatedMocks;
 };
+const mocksArray = createMocksArray();
 
 const showMap = function () {
   document.querySelector(`.map`).classList.remove(`map--faded`);
 };
 
-const createPins = function (generatedObject) {
-  const PIN_ELEMENT = PIN_TEMPLATE.cloneNode(true);
-  const PIN_ELEMENT_IMAGE = PIN_ELEMENT.querySelector(`img`);
-  const LEFT_COORDINATE = generatedObject.location.x + PIN_OFFSET_X;
-  const TOP_COORDINATE = generatedObject.location.y + PIN_OFFSET_Y;
-  PIN_ELEMENT.style = `
-  left: ${LEFT_COORDINATE}px;
-  top: ${TOP_COORDINATE}px;`;
-  PIN_ELEMENT_IMAGE.src = `${generatedObject.author.avatar}`;
-  PIN_ELEMENT_IMAGE.alt = `${generatedObject.offer.title}`;
-  return PIN_ELEMENT;
+const createPins = function (generatedMock) {
+  const pinElement = pinTemplate.cloneNode(true);
+  const pinElementImage = pinElement.querySelector(`img`);
+  const leftCoordinate = generatedMock.location.x + PIN_OFFSET_X;
+  const topElement = generatedMock.location.y + PIN_OFFSET_Y;
+  pinElement.style = `
+  left: ${leftCoordinate}px;
+  top: ${topElement}px;`;
+  pinElementImage.src = `${generatedMock.author.avatar}`;
+  pinElementImage.alt = `${generatedMock.offer.title}`;
+  return pinElement;
 };
 
 const renderPins = function () {
-  const PINS_FRAGMENT = document.createDocumentFragment();
-  for (let i = 0; i < generatedObjects.length; i++) {
-    PINS_FRAGMENT.appendChild(createPins(generatedObjects[i]));
+  const pinfragment = document.createDocumentFragment();
+  for (let i = 0; i < mocksArray.length; i++) {
+    pinfragment.appendChild(createPins(mocksArray[i]));
   }
-  PINS_MAP.appendChild(PINS_FRAGMENT);
+  pinsMap.appendChild(pinfragment);
 };
 
-createObjectArray();
 showMap();
 renderPins();
